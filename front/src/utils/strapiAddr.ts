d110a4633fe6dcb86f8d44c4b504bbcbd9daf778
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2021-01-29 20:20:56 -0300

    Production frontend

--- front/src/utils/strapiAddr.ts
+++ front/src/utils/strapiAddr.ts
@@ -1,4 +1,7 @@
-export const strapiAddr =
+/*export const strapiAddr =
   process.env.NODE_ENV == 'development' ? 'http://localhost:1341' :
   'https://bioinfo.imd.ufrn.br/aulas-api'
 
+*/
+
+export const strapiAddr = 'https://bioinfo.imd.ufrn.br/aulas-api'
